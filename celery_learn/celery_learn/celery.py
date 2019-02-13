d98ff2ae664940a34203cced98f812280b1d5169
Nikita <humapen@gmail.com>
2019-02-13 20:53:15 +0200

    added email resent

--- celery_learn/celery_learn/celery.py
+++ celery_learn/celery_learn/celery.py
@@ -1,7 +1,11 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 # https://code.tutsplus.com/ru/tutorials/using-celery-with-django-for-background-task-processing--cms-28732
+# $ redis-server
+# MAC $ celery worker -A celery_learn --loglevel=debug --concurrency=4
+# WINDOWS celery worker -A celery_learn --loglevel=debug --concurrency=4
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_learn.settings')
 
@@ -9,4 +13,4 @@ app = Celery('celery_learn')
 app.config_from_object('django.conf:settings')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks(settings.INSTALLED_APPS, related_name='tasks')
